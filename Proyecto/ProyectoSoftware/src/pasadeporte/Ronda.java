864ba202d59cf471aba7433224d2045e9c505e0a
unknown <joaquinruiz@uma.es>
2022-05-21 10:42:12 +0200

    Actualizo Ronda y Ronda Test

--- Proyecto/ProyectoSoftware/src/pasadeporte/Ronda.java
+++ Proyecto/ProyectoSoftware/src/pasadeporte/Ronda.java
@@ -1,5 +1,26 @@
 package pasadeporte;
 
+import java.util.Random;
+
 public class Ronda {
+	private int rondaBonus;
+	private int numero;
+	private Random r = new Random();
+	
+	public void generarRondaBonus() {
+		rondaBonus=r.nextInt(27);
+	}
+	
+	public int getRondaBonus() {
+		return rondaBonus;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void procesoRonda() {
+		
+	}
 
 }

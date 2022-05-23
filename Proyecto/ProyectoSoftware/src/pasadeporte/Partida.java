ce641f1080794f1fb6db67e380b2799e0e301963
carlos ramirez <rrscarlos@uma.es>
2022-05-23 14:07:25 +0200

    Terminamos clase partida a falta de los test

--- Proyecto/ProyectoSoftware/src/pasadeporte/Partida.java
+++ Proyecto/ProyectoSoftware/src/pasadeporte/Partida.java
@@ -1,11 +1,15 @@
 package pasadeporte;
 
+import java.util.ArrayList;
+
 public class Partida {
-    private ArrayList letras = new ArrayList(25);
+    private ArrayList letras;
 	private int turno;
 	
 	
 	public Partida() {
+		letras= new ArrayList(26);
+		turno=1;
 	}
 	
 	private void getResultado(Jugador j1,Jugador j2) {
@@ -26,8 +30,20 @@ public class Partida {
 		}
 	}
 	
-	public void jugar(Jugador j1,Jugador j2,Ronda r) {
-		while(r.getNumero()<letras.size()) {
+	public void jugar(Jugador j1,Jugador j2,Ronda r,Temporizador t) {
+		int i=1;
+		while(r.getNumero()<=letras.size()) {
+			System.out.println("Ronda de la letra "+letras.get(i));
+			if(turno==1) {
+				r.procesoRonda(j1, j2, t);
+				turno=2;
+			}else {
+				r.procesoRonda(j2, j1, t);
+				turno=1;
+			}
+			
+		r.sigRonda();
+		i++;
 		}
 	}
 	

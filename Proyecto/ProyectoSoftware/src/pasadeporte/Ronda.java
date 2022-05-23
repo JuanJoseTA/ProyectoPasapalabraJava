58a3d52f9c22bca45ba08ab28bd33fd840ce5fe4
unknown <javierluke02@uma.es>
2022-05-23 13:49:41 +0200

    Clase Ronda

--- Proyecto/ProyectoSoftware/src/pasadeporte/Ronda.java
+++ Proyecto/ProyectoSoftware/src/pasadeporte/Ronda.java
@@ -24,29 +24,46 @@ public class Ronda {
 	public int getNumero() {
 		return numero;
 	}
-	
-	public void procesoRonda() {
+	public boolean esBonus() {
+		return rondaBonus==numero || rondaBonus==numero+1;
+	}
+	public void procesoRonda(Jugador principal,Jugador rebote,Temporizador t) {
 		Pregunta p=new Pregunta(numero);
 		String r = null;
-		Temporizador temp= new Temporizador();
 		p.preguntar();
-		temp.iniciar();
-		while(temp.tiempoPasado()<=15 && r==null) {
-		try (Scanner sc = new Scanner(System.in)) {
-			 r=sc.nextLine();
-		}catch(InputMismatchException i) {
-			System.err.println(i.getMessage());
+		t.iniciar();
+		while(t.tiempoPasado()<=15 && r==null) {
+		    try (Scanner sc = new Scanner(System.in)) {
+		    r=sc.nextLine();
+		    }catch(InputMismatchException i) {
+		    System.err.println(i.getMessage());
+		    }
+		    t.parar();
 		}
-		temp.parar();
-		if(!p.respuesta(r)){
-			System.out.println("INCORRECTO \n");
-			System.out.println("REBOTE \n");
-			temp.reiniciar();
-			temp.iniciar();
-			p.preguntar();
-			
-			
+		if(p.respuesta(r) && t.tiempoPasado()<=15){
+		    System.out.println("CORRECTO \n");
+		    principal.sumar(esBonus());
+		}else {
+			 System.out.println("INCORRECTO, HAY REBOTE\n");
+			 p.preguntar();
+			 t.reiniciar();
+			 t.iniciar();
+			 while(t.tiempoPasado()<=15 && r==null) {
+				    try (Scanner sc = new Scanner(System.in)) {
+				    r=sc.nextLine();
+				    }catch(InputMismatchException i) {
+				    System.err.println(i.getMessage());
+				    }
+				    t.parar();
+			}
+			 if(p.respuesta(r) && t.tiempoPasado()<=15){
+				    System.out.println("CORRECTO \n");
+				    rebote.sumar(esBonus());
+			  }else {
+				  System.out.println("INCORRECTO");
+				  System.out.println("La solucion correcta es: ");
+				  p.mostrarSolucion();
+			  }
 		}
 	}
-}
 }

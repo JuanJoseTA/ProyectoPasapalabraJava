1982957dcf30332d217cc92d575f5cc1fba13af5
carlos ramirez <rrscarlos@uma.es>
2022-05-30 13:35:10 +0200

    Terminamos test partida

--- Proyecto/ProyectoSoftware/src/pasadeporte/PartidaTest.java
+++ Proyecto/ProyectoSoftware/src/pasadeporte/PartidaTest.java
@@ -2,6 +2,8 @@ package pasadeporte;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,9 +11,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PartidaTest {
-
+	private static Partida p;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		assertNull(p.getTurno(),"exito");
+		assertNull(p.getLetras(),"exito");
 	}
 
 	@AfterAll
@@ -20,25 +24,44 @@ class PartidaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		p = new Partida();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		p = null;
 	}
 
 	@Test
 	void testPartida() {
-		fail("Not yet implemented");
+		assertNotNull(p.getTurno(),"exito");
+		assertNotNull(p.getLetras(),"exito");
 	}
 
 	@Test
 	void testIniciaLetras() {
-		fail("Not yet implemented");
+		ArrayList letras = p.getLetras();
+		boolean ok=true;
+		
+		for(int i=0;i<letras.size()&&!ok;i++) {
+			if(letras.get(i)==null) {
+				ok=false;
+			}
+		}
+		
+		assertTrue(ok,"exito");
 	}
 
 	@Test
 	void testJugar() {
-		fail("Not yet implemented");
+		boolean ok=true;
+		Ronda r;
+		
+		if(r.getNumero()<=p.getLetras().size()) {
+			ok=false;
+		}
+		
+		assertTrue(ok,"exito");
 	}
 
 }

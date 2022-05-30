e23a68680017ce5398056704a0c4329652f463bb
unknown <javierluke02@uma.es>
2022-05-30 13:32:28 +0200

    main

--- Proyecto/ProyectoSoftware/src/pasadeporte/Main.java
+++ Proyecto/ProyectoSoftware/src/pasadeporte/Main.java
@@ -2,6 +2,26 @@ package pasadeporte;
 
 public class Main {
     public static void main(String[] args) {
+    	String alias1;
+    	String alias2;
+    	Scanner teclado=new Scanner(System.in);
+    	System.out.print("Nombre del jugador 1: ");
+    	alias1=teclado.nextLine();
+    	System.out.print("\nNombre del jugador 2: ");
+    	alias2=teclado.nextLine();
+    	
+    	Jugador j1= new Jugador(alias1);
+    	Jugador j2=new Jugador(alias2);
+    	Temporizador t=new Temporizador();
+    	Ronda r=new Ronda();
+    	Partida partida=new Partida();
+    	
+    	
+    	partida.iniciaLetras();
+    	partida.jugar(j1, j2,r, t);
+    	
+    	partida.getResultado(j1, j2);
+    	
     	
     	
 	}
